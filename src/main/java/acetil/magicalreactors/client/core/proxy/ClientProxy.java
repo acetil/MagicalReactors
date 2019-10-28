920720a36ee470302dea17baf69ba8d993528aac
acetil <acetildev@gmail.com>
2019-10-29 10:30:52 +1100

    Removed old proxy code

--- src/main/java/acetil/magicalreactors/client/core/proxy/ClientProxy.java
+++ src/main/java/acetil/magicalreactors/client/core/proxy/ClientProxy.java
@@ -1,39 +1,14 @@
 package acetil.magicalreactors.client.core.proxy;
 
 import acetil.magicalreactors.common.core.proxy.IProxy;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import acetil.magicalreactors.client.gui.json.MachineGuiManager;
-import acetil.magicalreactors.common.MagicalReactors;
-import acetil.magicalreactors.common.block.ModBlocks;
-import acetil.magicalreactors.common.items.ModItems;
 
-@Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy implements IProxy {
-    @Override
-    public void preInit(FMLPreInitializationEvent event) {
 
-    }
-
-    @Override
+    /*@Override
     public void init(FMLInitializationEvent event) {
         NetworkRegistry.INSTANCE.registerGuiHandler(MagicalReactors.instance, new GuiProxy());
         MachineGuiManager.readGuiJson("assets/nuclearmod/gui");
-    }
+    }*/
 
-    @Override
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
-    @SubscribeEvent
-    public static void registerModels(ModelRegistryEvent event) {
-        ModItems.initModels();
-        ModBlocks.initModels();
-    }
 }
+
--- src/main/java/acetil/magicalreactors/common/core/proxy/IProxy.java
+++ src/main/java/acetil/magicalreactors/common/core/proxy/IProxy.java
@@ -1,9 +1,6 @@
 package acetil.magicalreactors.common.core.proxy;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod.EventBusSubscriber
 public interface IProxy {
