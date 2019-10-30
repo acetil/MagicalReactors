f9abd19736a89f3cf27599109fb1794c9d5e57ec
acetil <acetildev@gmail.com>
2019-10-31 10:00:02 +1100

    Added wip rune texture and fixed blockstates

--- src/main/java/acetil/magicalreactors/common/block/reactor/BlockRuneBase.java
+++ src/main/java/acetil/magicalreactors/common/block/reactor/BlockRuneBase.java
@@ -3,7 +3,10 @@ package acetil.magicalreactors.common.block.reactor;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.Item;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -12,47 +15,24 @@ import acetil.magicalreactors.common.core.NuclearCreativeTab;
 import acetil.magicalreactors.common.lib.LibMisc;
 
 public class BlockRuneBase extends Block implements IReactorBuildingBlock {
-    //private static final PropertyBool MULTIBLOCK_STATE = PropertyBool.create("completed_multiblock");
+    private static final BooleanProperty MULTIBLOCK_STATE = BooleanProperty.create("completed_multiblock");
     public BlockRuneBase(String registryName) {
         super(Properties.create(Material.ROCK));
         setRegistryName(registryName);
-        //setDefaultState(this.getBlockState().getBaseState().withProperty(MULTIBLOCK_STATE, false));
+        setDefaultState(getStateContainer().getBaseState().with(MULTIBLOCK_STATE, false));
     }
-    /*@Override
-    public BlockStateContainer createBlockState () {
-        return new BlockStateContainer(this, MULTIBLOCK_STATE);
-    }*/
+
     @Override
     public void updateMultiblockState(BlockState state, World worldIn, BlockPos pos, boolean isMultiblock) {
-        /*if (isMultiblock && !state.getValue(MULTIBLOCK_STATE)) {
-            worldIn.setBlockState(pos, state.withProperty(MULTIBLOCK_STATE, true));
-        } else if (!isMultiblock && state.getValue(MULTIBLOCK_STATE)) {
-            worldIn.setBlockState(pos, state.withProperty(MULTIBLOCK_STATE, false));
-        }*/
-    }
-    public enum EnumMultiblockState implements IStringSerializable {
-        NOT_IN_MULTIBLOCK("not_in_multiblock"), IN_MULTIBLOCK("in_multiblock");
-        private String name;
-        EnumMultiblockState (String name) {
-            this.name = name;
-        }
-        @Override
-        public String getName () {
-            return name;
-        }
-        public int getBits () {
-            if (this.name.equals("in_multiblock")) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-        public static EnumMultiblockState getStateFromBits (int bits) {
-            if ((bits | 1) == 1) {
-                return IN_MULTIBLOCK;
-            } else {
-                return NOT_IN_MULTIBLOCK;
-            }
+        if (isMultiblock && !state.get(MULTIBLOCK_STATE)) {
+            worldIn.setBlockState(pos, state.with(MULTIBLOCK_STATE, true));
+        } else if (!isMultiblock && state.get(MULTIBLOCK_STATE)) {
+            worldIn.setBlockState(pos, state.with(MULTIBLOCK_STATE, false));
         }
     }
+    @Override
+    protected void fillStateContainer (StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(MULTIBLOCK_STATE);
+    }
+
 }
