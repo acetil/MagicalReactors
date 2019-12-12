3cd0f82401c01eee8f8633cb6ebad231a3b2ae6b
acetil <acetildev@gmail.com>
2019-12-13 02:18:34 +1100

    Removed ModifiableSupplier

--- src/main/java/acetil/magicalreactors/common/constants/MachineConfig.java
+++ src/main/java/acetil/magicalreactors/common/constants/MachineConfig.java
@@ -9,9 +9,6 @@ public class MachineConfig {
     public Supplier<Integer> ENERGY_STORAGE;
     public Supplier<Integer> MAX_RECEIVE;
     public Supplier<Integer> ENERGY_USE_RATE;
-    ModifiableSupplier<Integer> energyStorageModifiable;
-    ModifiableSupplier<Integer> maxReceiveModifiable;
-    ModifiableSupplier<Integer> energyUseRateModifiable;
     public int defaultEnergyStorage;
     public int defaultMaxReceive;
     public int defaultEnergyUseRate;
@@ -20,11 +17,8 @@ public class MachineConfig {
         this.defaultEnergyStorage = defaultEnergyStorage;
         this.defaultMaxReceive = defaultMaxReceive;
         this.defaultEnergyUseRate = defaultEnergyUseRate;
-        energyStorageModifiable = new ModifiableSupplier<>(defaultEnergyStorage);
-        maxReceiveModifiable = new ModifiableSupplier<>(defaultMaxReceive);
-        energyUseRateModifiable = new ModifiableSupplier<>(defaultEnergyUseRate);
-        ENERGY_STORAGE = energyStorageModifiable::get;
-        MAX_RECEIVE = maxReceiveModifiable::get;
-        ENERGY_USE_RATE = energyUseRateModifiable::get;
+        ENERGY_STORAGE = () -> defaultEnergyStorage;
+        MAX_RECEIVE = () -> defaultMaxReceive;
+        ENERGY_USE_RATE = () -> defaultEnergyUseRate;
     }
 }
